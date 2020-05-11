ab38410bcbbaea26fca2002f4a02063ac774eba3
salah1337 <salah1337@protonmail.com>
2020-05-11 02:31:53 +0000

    Refactored show subscription view.

--- resources/views/subscription/show.blade.php
+++ resources/views/subscription/show.blade.php
@@ -8,23 +8,37 @@
         <br/>
         Subscription price: {{ $Subscription->price }}
         <br/>
-        @if ( $Subscription->status != "Canceled" )
-            Subscription created on: {{ $Subscription->createdAt }}
-            <br/>
-            @if ($Subscription->trialPeriod)
-                Subscription is on {{ $Subscription->trialDuration }} {{ $Subscription->trialDurationUnit }} trial period.
-                The free trial will end on {{ $Subscription->firstBillingDate }}, if you do not wish to be billed, cancel before then.
-            @else
-                Your Subscription is active, you will be billed on the {{ $Subscription->billingDayOfMonth }}th of each month.
-            @endif
-            <br/>
-            <br/>
-            <a class="btn btn-primary" href="{{ URL::previous() }}">Back</a>
-            <a class="btn btn-warning" href="/subscription/edit/{{ $mySubscription->id }}">Change Subscription</a>
-            <a class="btn btn-danger" href="/subscription/cancel/{{ $mySubscription->id }}">Cancel</a>
-        @else
-            <a class="btn btn-primary" href="{{ URL::previous() }}">Back</a>
-            <a class="btn btn-success" href="/subscription/create">Subscribe Again</a>
-        @endif
+        @switch($Subscription->status)
+            @case("Active")
+                    Subscription created on: {{ $Subscription->createdAt }}
+                    <br/>
+                    @if ($Subscription->trialPeriod)
+                        Subscription is on {{ $Subscription->trialDuration }} {{ $Subscription->trialDurationUnit }} trial period.
+                        The free trial will end on {{ $Subscription->firstBillingDate }}, if you do not wish to be billed, cancel before then.
+                    @else
+                        Your Subscription is active, you will be billed on the {{ $Subscription->billingDayOfMonth }}th of each month.
+                    @endif
+                    <br/>
+                    <br/>
+                    <a class="btn btn-primary" href="{{ URL::previous() }}">Back</a>
+                    <a class="btn btn-danger" href="/subscription/cancel/{{ $mySubscription->id }}">Cancel</a>
+                    <br/>
+                    @if (!$Subscription->trialPeriod)
+                        <a class="btn btn-warning" href="/subscription/edit/{{ $mySubscription->id }}">Upgrade / Downgrade</a>
+                    @endif
+                @break
+            @case("Canceled")
+                    <a class="btn btn-primary" href="{{ URL::previous() }}">Back</a>
+                    <a class="btn btn-success" href="/subscription/create">Subscribe Again</a>
+                @break
+            @case("Pending")
+                    Subscription is pending untill {{ $Subscription->firstBillingDate }}.
+                    <br/>
+                    <a class="btn btn-danger" href="/subscription/cancel/{{ $mySubscription->id }}">Cancel</a>
+                    <a class="btn btn-primary" href="{{ URL::previous() }}">Back</a>
+                @break
+            @default
+                
+        @endswitch
     </div>
 @endsection
