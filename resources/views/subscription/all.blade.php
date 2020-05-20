27ba86e86c758b487a3c2eafe87d94fb04f4df33
salah1337 <salah1337@protonmail.com>
2020-05-20 00:54:10 +0000

    Added show all Subs View.

--- resources/views/subscription/all.blade.php
+++ resources/views/subscription/all.blade.php
@@ -5,7 +5,8 @@
     @if (!$subscriptions)
         <h1>Something went wrong here. <a href='/contact'> Contact us </a> </h1>
     @endif
-    @foreach ($subscriptions as $subscription)
+    <div class="container grid-box">
+        @foreach ($subscriptions as $subscription)
         <div class="card">
             <div class="card-body">
             <h5 class="card-title">{{ $subscription->planId }}</h5>
@@ -13,5 +14,21 @@
             <a href="/subscription/show/{{ $subscription->id }}" class="btn btn-primary">More</a>
             </div>
         </div>
-    @endforeach
+        @endforeach
+    </div>
+@endsection
+
+
+@section('css')
+    <style>
+
+      .grid-box{
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+          grid-auto-rows: repeat(auto-fill, minmax(250px, 1fr));
+          grid-column-gap: 10px;
+          grid-row-gap: 10px;
+      }
+
+    </style>
 @endsection
